dd38053fee28bf3ba6028aab2f766555f9f9e19d
Fin <72807493+FinTestin@users.noreply.github.com>
2024-07-26 17:30:29 +0800

    Update DataDisplay.dart

--- Flutter/lib/DataDisplay.dart
+++ Flutter/lib/DataDisplay.dart
@@ -148,7 +148,7 @@ class _DisplayPageState extends State<DisplayPage> with SingleTickerProviderStat
           ),
           Container(
             padding: const EdgeInsets.only(bottom: 50),
-            child: Text('${systemText('nowtempis')}'
+            child: Text(S.of(context).nowtempis'
                 '${_esp.temp.toStringAsFixed(1)} ${systemText('°C')}',
               style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30* textSizeChange),
             ),
